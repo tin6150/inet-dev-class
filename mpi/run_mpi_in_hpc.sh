6ad6ff8c6676ade5ccfda21798434a9ad2484051
sn <tin6150@gmail.com>
2017-01-23 20:27:17 -0500

    minor fix

--- mpi/run_mpi_in_hpc.sh
+++ mpi/run_mpi_in_hpc.sh
@@ -3,9 +3,8 @@
 #$ -cwd
 #$ -j y
 #$ -S /bin/bash
-#$ -M tin.ho@novartis.com
-## #$ -pe orte 4    # has 0 slot, something not configured?
-#$ -pe openmpi 4
+#$ -pe orte 4    # has 0 slot, something not configured?
+## #$ -pe openmpi 4
 #$ -l m_mem_free=1G
 #$ -l h_rt=600
   
@@ -14,7 +13,8 @@
   #
   export MODULEPATH=/usr/prog/modules/all:$MODULEPATH
   module purge
-  module load openmpi/open64/64/1.8.1
+  #module load openmpi/open64/64/1.8.1
+  module load OpenMPI/1.6.5-GCC-4.8.2
 
   # Execute the run
-  mpirun -np 4 sh ./worker_script.sh
+  mpirun -np 4 ./a.out
