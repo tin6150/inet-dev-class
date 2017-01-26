d4841b6af408d4ac16cab1257df82fec1f42d38a
tin6150 <tin6150@gmail.com>
2017-01-25 20:47:24 -0800

    working script for qsub

--- mpi/run_mpi_in_hpc.sh
+++ mpi/run_mpi_in_hpc.sh
@@ -3,7 +3,7 @@
 #$ -cwd
 #$ -j y
 #$ -S /bin/bash
-#$ -pe orte 4    # has 0 slot, something not configured?
+#$ -pe orte 4    
 ## #$ -pe openmpi 4
 #$ -l m_mem_free=1G
 #$ -l h_rt=600
@@ -11,10 +11,10 @@
   #
   # Use modules to setup the runtime environment
   #
-  export MODULEPATH=/usr/prog/modules/all:$MODULEPATH
+  export MODULEPATH=/prog/modules/all:$MODULEPATH
   module purge
   #module load openmpi/open64/64/1.8.1
-  module load OpenMPI/1.6.5-GCC-4.8.2
+  module load OpenMPI/1.6.5-GCC-4.8.2-X
 
   # Execute the run
   mpirun -np 4 ./a.out
