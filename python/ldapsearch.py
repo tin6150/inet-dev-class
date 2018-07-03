7ee8efac93440513dc301df173aba3267478c4b6
tin6150 <tin@cueball>
2018-07-03 14:59:32 -0700

    working ldapsearch in python 2

--- python/ldapsearch.py
+++ python/ldapsearch.py
@@ -1,29 +1,54 @@
 #!/usr/bin/env python
 
-# ref: https://www.adimian.com/blog/2014/10/basic-ldap-actions-using-python/
-# pip install python-ldap
+# example function to search ldap for user
+#
+# some strange problem with python3, so sticking with python2 for now.
+# ref: 
+# https://www.adimian.com/blog/2014/10/basic-ldap-actions-using-python/
 # https://www.python-ldap.org/en/latest/installing.html#build-prerequisites
+# https://www.python-ldap.org/en/latest/reference/ldap.html?highlight=initialize
+
+
+# one-time setup:
+# module load python/3.5
+# pyvenv ~/local_python_35
+# source ~/local_python_35/bin/activate
+# pip install python-ldap
+# pip uninstall ldap # if it complains don't have ldap.initialize()
+
+
 import ldap
 
 
 def found_in_ldap( user ):
+		#print ldap.__file__
 		con = ldap.initialize('ldap://identity.lbl.gov:389')
-
 		ldap_base = "ou=people,dc=lbl,dc=gov"
-		#query = "(uid=maarten)"
-		query = "(uid=%s)" % user
-		result = con.search_s(ldap_base, ldap.SCOPE_SUBTREE, query)
-		return 0
-		#print( result )
-		#retrun True 
-		#exit 0
+		#searchAttribute = ["uid","cn","mail"]
+		searchAttribute = ["mail"]
+		#query = "(uid=wavalos)"
+		query = "(uid=%s)" % user		# uid is loginname eg "wavalos"
+		#query = "(sn=%s)" % user		# sn is "surname"
+		#query = "(givenname=%s)" % user
+		#result_set = con.search_s(ldap_base, ldap.SCOPE_SUBTREE, query)    # unfiltered results
+		result_set = con.search_s(ldap_base, ldap.SCOPE_SUBTREE, query, searchAttribute)
+		print( result_set )
+		print( len(result_set) )	# number of matches
+		#return result_set		# could return full result for caller to handle if desired
+		return len(result_set)
 # END found_in_ldap( user )
 
 
 
 def main():
-    found_in_ldap("wavalos")
-    return 7 
+    found_in_ldap("wendy")
+    result_set = found_in_ldap("tin")
+    print( result_set )
+    print( "===" )
+    found_in_ldap("foobar")
+    if found_in_ldap("wavalos") :
+       print( "found wavalos" )
+    return 0 
 # END main()
 
 
