f6569ff8869d922f30d6166e6158e6699e065ba6
tin6150 <tin@cueball>
2018-07-23 17:36:39 -0700

    python ldapsearch for alias + cleaned comments

--- python/ldapsearch.py
+++ python/ldapsearch.py
@@ -20,31 +20,30 @@
 
 import ldap
 
+# this only look for the name as alias in ldap
 def found_alias_in_ldap( user ):
 		con = ldap.initialize('ldap://identity.lbl.gov:389')
 		ldap_base = "ou=people,dc=lbl,dc=gov"
 		#searchAttribute = ["uid","cn","lblGoogleAccountName", "lblAccountStatus"]
 		searchAttribute = ["lblGoogleAccountName"]
-		#searchAttribute = ["mail"]
-		query = "(lblAccountUsernameAlias=%s)" % user		# 
+		query = "(lblAccountUsernameAlias=%s)" % user		
 		#result_set = con.search_s(ldap_base, ldap.SCOPE_SUBTREE, query)    # unfiltered results
 		result_set = con.search_s(ldap_base, ldap.SCOPE_SUBTREE, query, searchAttribute)
-		#print( result_set )
-		#print( len(result_set) )	# number of matches
-		if( len(result_set) > 0 ) :
-			#return result_set[0]
+		if( len(result_set) > 0 ) :         # see if there are any matches
 			(a,b) = result_set[0]
 			#print( "a==>%s<==" % a )
 			#print( "b==>%s<==" % b )
-			print( "dict==>%s<==" % b['lblGoogleAccountName'] )
+			#print( "dict==>%s<==" % b['lblGoogleAccountName'] )
 			return b['lblGoogleAccountName']
 		else :
-			#return "Username Not Found"
+			#return "Alias Not Found"
+			#return len(result_set) 	# number of matches
 			return False
 # END found_alias_in_ldap( user )
 
 
 
+# search for username exactly as spelled in ldap
 def found_in_ldap( user ):
 		#print ldap.__file__
 		con = ldap.initialize('ldap://identity:389')
@@ -52,21 +51,18 @@ def found_in_ldap( user ):
 		#searchAttribute = ["uid","cn","mail"]
 		#searchAttribute = ["mail"]
 		searchAttribute = ["lblGoogleAccountName"]
-		#query = "(uid=wavalos)"
 		query = "(uid=%s)" % user		# uid is loginname eg "wavalos"
 		#query = "(sn=%s)" % user		# sn is "surname"
 		#query = "(givenname=%s)" % user
 		#result_set = con.search_s(ldap_base, ldap.SCOPE_SUBTREE, query)    # unfiltered results
 		result_set = con.search_s(ldap_base, ldap.SCOPE_SUBTREE, query, searchAttribute)
-		print( result_set )
-		#print( len(result_set) )	# number of matches
-		
-		if( len(result_set) > 0 ) :
-			#return result_set[0]
+		#print( result_set )
+		#print( len(result_set) )	# number of matches, depending on query, could have more than 1 match
+		if( len(result_set) > 0 ) :     
 			(a,b) = result_set[0]
 			#print( "a==>%s<==" % a )
 			#print( "b==>%s<==" % b )
-			print( "dict==>%s<==" % b['lblGoogleAccountName'] )
+			#print( "dict==>%s<==" % b['lblGoogleAccountName'] )
 			return b['lblGoogleAccountName']
 		else :
 			#return "Username Not Found"
@@ -97,12 +93,12 @@ def main():
     print( "*************" )
 
 
-    result = found_alias_in_ldap("mhg")
-    print( result_set )
 
     print( "==Looking for tin==" )
     result = found_in_ldap("tin")
     print( "got response of: %s" % result )
+    result = found_alias_in_ldap("tin")
+    print( result )
 
     print( "==Looking for foobar==" )
     result = found_in_ldap("foobar")
