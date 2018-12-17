d0c1e4cdf968d807130594fb9d04f147527d7043
tin6150 <tin6150@gmail.com>
2018-12-16 19:52:53 -0800

    flask app with blueprints as router for collection of views (really means putting the route commands in views.py, mostly?)

--- flask-mapbox/app_map.py
+++ flask-mapbox/app_map.py
@@ -1,14 +1,17 @@
 #!/bin/env/python 
 # using python 2.7
 
-## flask app to load mapbox studio styled map
-## start with things that i had previously "published" via html in github.io
-## based on app.py (ie, mostly barebone Hello World code)
-## structuring after Tyler biositting templates/mbgl_index.html
+"""
+flask app to load mapbox studio styled map
+start with things that i had previously "published" via html in github.io
+structuring after Tyler biositting templates/mbgl_index.html
+"""
 
 
 import os
 from flask import Flask
+from views import master	# this likely the one that loads views.py
+
 from flask_sqlalchemy import SQLAlchemy
 
 def create_app(test_config=None): 
@@ -23,19 +26,27 @@ def create_app(test_config=None):
 	#config = os.environ['DATABASE_URL'] # biositting_tools get this from OS ENV
 	#db = SQLAlchemy(app)
 	
+	# Tyler Biositting approach 
+	app.register_blueprint(master, url_prefix='')
+	#app.config.from_object(config)
+	#register_extensions(app)
+	#register_shellcontext(app)
+
+
 	# do a "Hello World"
 	# ref http://flask.pocoo.org/docs/1.0/tutorial/factory/
 	# below register app to http://bofh.lbl.gov:5000/hello
-	@app.route('/hello')
-	def hello():
-		return "Hello, World! (at /hello)"
+	@app.route('/hi')
+	def hi():
+		"""Print 'hello world' as the response body."""
+		return "Hi, World! (at /hi)"
 
+	# Tyler used blueprint to register bunch of views rather than defining them here.
 	# https://opensource.com/article/18/4/flask
 	# as main app : 
-	@app.route('/')  # register to top of web server
-	def hello_world():
-		"""Print 'Hello, world!' as the response body."""
-		return "Hello, World! (at /)"
+	#@app.route('/')  # register to top of web server
+	#def hello_world():
+		#return "Hello, World! (at /)"
 
 	return app
 #end create_app()
@@ -47,7 +58,7 @@ def create_app(test_config=None):
 if __name__ == '__main__':
     app = create_app()
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     #app.run(port=port)				# this is default, localhost only
     app.run(host='0.0.0.0', port=port)		# i have iptables limiting exposure
 
