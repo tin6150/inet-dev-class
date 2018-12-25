11c11648484622db01b224352843657274cac9c5
tin6150 <tin6150@gmail.com>
2018-12-25 13:56:20 -0800

    still trying to get import of mapbox plugin to work

--- js/mapFromFile.js
+++ js/mapFromFile.js
@@ -14,9 +14,11 @@ var fs = require('fs');
 
 var server = http.createServer(function (req, res) {
   //fs.readFile('ZWEDC_50x50sq.html', 		// work with this one
-  fs.readFile('ZWEDC_50x50sq+plugin.html', 	// fail here, either w/ or w/o "import clause
+  fs.readFile('ZWEDC_50x50sq+Plugin.html', 	// fail here, either w/ or w/o "import clause
   function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    //res.writeHead(200, {'Content-Type': 'text/html'});
+    //res.writeHead(200, {'Content-Type': 'application/javascript'});
+    res.writeHead(200, {'Content-Type': 'text'});
     res.write(data);
     res.end();
   });
