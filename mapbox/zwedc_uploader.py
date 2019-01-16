37c54ab4c8ca479360b8cace512e530c80e9100b
tin6150 <sn+newbox@grumpyxmas.com>
2019-01-16 08:21:11 -0800

    seeding python uploader for mapbox

---
+++ mapbox/zwedc_uploader.py
@@ -0,0 +1,18 @@
+#!/bin/env python 
+
+# python script to upload geojson for ZWEDC into mapbox, where it would convert them as tileset
+# tin 2019.01.16
+
+# https://github.com/mapbox/mapbox-sdk-py/blob/master/docs/uploads.md
+# need mapbox python api, from parent dir of above link.
+
+# https://www.mapbox.com/api-documentation/maps/#uploads
+# need to have mapbox secret token to generate temp aws key for stage file in S3, see area51
+
+from mapbox import Uploader
+
+service = Uploader()
+from time import sleep
+from random import radint
+mapid = getfixure('uploads_dest_id') # upload-test
+
