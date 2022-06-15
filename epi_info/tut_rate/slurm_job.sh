f5d0f19246a23c9c998ce8d12410ca455e7e16cf
tin6150 <tin@brcmst>
2022-06-15 00:12:45 -0700

    shell script to run beast with xml, edit chainLegth that affects run time, prepping to run as slurm job

--- epi_info/tut_rate/slurm_job.sh
+++ epi_info/tut_rate/slurm_job.sh
@@ -15,14 +15,56 @@ module load cmake/3.22.0    # from consultsw
 module load java/1.8.0_121
 module load cuda/11.2
 
+PID=$$
+##[[ -f YFV.log.txt ]] && mv YFV.log.txt YFV.log.txt.old.$PID
+## hopefully can just use -overwrite
 
-## may need to cd
-## and check path...
-cd /global/home/users/tin/tin-gh/inet-dev-class/epi_info/tut_rate
+run_rate_tut_cd_JAR_DIR() {
 
+	BEAST_XML=/global/home/users/tin/tin-gh/inet-dev-class/epi_info/tut_rate/SnYFV.xml
+	BEAST_JAR_DIR=/global/home/users/tin/tin-gh/inet-dev-class/epi_info/travHistProtocol/software/
+	cd $BEAST_JAR_DIR
+	java -cp beast.jar dr.app.beast.BeastMain -seed 2020 -beagle_double -beagle_gpu -save_every 1000000 -save_state YFV.checkpoint   $BEAST_XML
 
+}
+
+#### prefer to run on dir with input XML instead of cd to the jar dir.
+#### so using the method below instead
 BEAST_JAR=/global/home/users/tin/tin-gh/inet-dev-class/epi_info/travHistProtocol/software/beast.jar
 
-java -cp beast.jar dr.app.beast.BeastMain -seed 2020 -beagle_double -beagle_gpu -save_every 1000000 -save_state YFV.checkpoint   \
- ./SnYFV.xml
- ##  ../files/Protocol3/282_GISAID_sarscov2_travelHist_masked.xml   # cranking on n0262.s4, cuda 11.2
+run_rate_tut() {
+
+	#### to make it run longer or shorter, change XML param chainLength="10000"
+
+	cd /global/home/users/tin/tin-gh/inet-dev-class/epi_info/tut_rate
+	java -cp $BEAST_JAR dr.app.beast.BeastMain -seed 2020 -beagle_double -beagle_gpu -save_every 1000000 -save_state Beast.chkpt -overwrite   ./SnYFV.xml
+
+	exit $?
+}
+
+run_SARS_CoV2() {
+	##++ may try to rerun below in V100 as slurm job with the orig 200M chainLegth
+	cd /global/home/users/tin/tin-gh/inet-dev-class/epi_info/travHistProtocol/files/Protocol3
+    BEAST_XML=282_GISAID_sarscov2_travelHist_masked_Sn100k.xml
+	java -cp $BEAST_JAR dr.app.beast.BeastMain -seed 2020 -beagle_double -beagle_gpu -save_every 1000000 -save_state Beast.chkpt    $BEAST_XML
+## ../files/Protocol3/282_GISAID_sarscov2_travelHist_masked.xml   # cranking on n0262.s4, cuda 11.2
+##travelHist.checkpoint ../files/Protocol3/282_GISAID_sarscov2_travelHist_masked.xml
+
+}
+
+
+
+
+####
+#### "main" here
+####
+echo Starting the BEAST
+date
+
+#run_rate_tut
+run_SARS_CoV2  ## chainLegth=100k  for now , n0264.s3
+
+echo $?
+echo BEAST done
+date
+
