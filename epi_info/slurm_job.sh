0570df254f47c488b3e532ee5d3fcc04774b1b4b
tin6150 <tin@brcmst>
2022-06-16 17:11:24 -0700

    updated job running script. sav3

--- epi_info/slurm_job.sh
+++ epi_info/slurm_job.sh
@@ -2,10 +2,12 @@
 
 ## slurm job script for running BEAST v1 
 ## in dev
+## for now it is just a bash script for run directly on host
+## eg run:
+## n264s3>  bash -x slurm_job.sh | tee tut_flu/beast_job.OUT.TXT
 
-
-# see README.rst and PDF for commands to run
 # BEAAST processing xml (java program, need beagle lib, which could use accel from CUDA)
+# ref README.rst and PDF for commands to run
 
 export LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$HOME/lib/pkgconfig:$PKG_CONFIG_PATH
@@ -16,32 +18,38 @@ module load java/1.8.0_121
 module load cuda/11.2
 
 PID=$$
-##[[ -f YFV.log.txt ]] && mv YFV.log.txt YFV.log.txt.old.$PID
-## hopefully can just use -overwrite
 
+## I dont like this method and no longer used
 run_rate_tut_cd_JAR_DIR() {
-
 	BEAST_XML=/global/home/users/tin/tin-gh/inet-dev-class/epi_info/tut_rate/SnYFV.xml
 	BEAST_JAR_DIR=/global/home/users/tin/tin-gh/inet-dev-class/epi_info/travHistProtocol/software/
 	cd $BEAST_JAR_DIR
 	java -cp beast.jar dr.app.beast.BeastMain -seed 2020 -beagle_double -beagle_gpu -save_every 1000000 -save_state YFV.checkpoint   $BEAST_XML
-
 }
 
 #### prefer to run on dir with input XML instead of cd to the jar dir.
 #### so using the method below instead
 BEAST_JAR=/global/home/users/tin/tin-gh/inet-dev-class/epi_info/travHistProtocol/software/beast.jar
 
-run_rate_tut() {
-
+## tutorial 1: Rate
+run_tut_rate() {
 	#### to make it run longer or shorter, change XML param chainLength="10000"
-
 	cd /global/home/users/tin/tin-gh/inet-dev-class/epi_info/tut_rate
 	java -cp $BEAST_JAR dr.app.beast.BeastMain -seed 2020 -beagle_double -beagle_gpu -save_every 1000000 -save_state Beast.chkpt -overwrite   ./SnYFV.xml
-
 	exit $?
 }
 
+
+## tutorial 2: Influenza evo
+run_tut_flu() {
+    BEAST_XML=Sn_H1N1pdm_2009.xml
+	cd /global/home/users/tin/tin-gh/inet-dev-class/epi_info/tut_flu
+	java -cp $BEAST_JAR dr.app.beast.BeastMain -seed 2020 -beagle_double -beagle_gpu -save_every 1000000 -save_state Beast.chkpt -overwrite   $BEAST_XML
+	exit $?
+}
+
+
+## the orig COVID paper with chainLength=200M  
 run_SARS_CoV2() {
 	##++ may try to rerun below in V100 as slurm job with the orig 200M chainLegth
 	cd /global/home/users/tin/tin-gh/inet-dev-class/epi_info/travHistProtocol/files/Protocol3
@@ -58,13 +66,16 @@ run_SARS_CoV2() {
 ####
 #### "main" here
 ####
-echo Starting the BEAST
 date
+echo "====INFO===="
+java -cp $BEAST_JAR dr.app.beast.BeastMain -beagle_info
 
-#run_rate_tut
-run_SARS_CoV2  ## chainLegth=100k  for now , n0264.s3
+echo "====Starting the BEAST===="
+#run_tut_rate
+#run_SARS_CoV2  ## chainLegth=100k  for now , n0264.s3
+run_tut_flu
 
 echo $?
-echo BEAST done
+echo "====BEAST done===="
 date
 
